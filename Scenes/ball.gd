0641a220409d100a632cf71950be8c516dc81b2d
Rafael Diaz de Leon <r@rdleon.me>
2023-10-03 10:35:10 -0700

    Fix collision dragging the paddle

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -1,11 +1,19 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+const SPEED = 300
+
+var motion = Vector2()
+var left = Vector2(1, 0)
+var right = Vector2(-1, 0)
+var direction = left
+
+func _ready():
+	rotation = 1.0
+	position = Vector2(644, 347)
+	velocity = Vector2(SPEED, 0).rotated(rotation)
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	apply_impulse(Vector2(-100,0))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-@export var SPEED : int = 30
+@export var SPEED : int = 300
 
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_up", "ui_down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = 0
 
-	move_and_collide(velocity)
+	move_and_slide()
