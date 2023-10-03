5f1c941d1930a19cbe8cd81d78d42b1969fcd7df
Javier Plata <silver9ga@gmail.com>
2023-10-03 13:29:48 -0500

    changed the walls to interact with the ball when entered

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -1,5 +1,4 @@
 extends CharacterBody2D
-
 const SPEED = 300
 
 var motion = Vector2()
@@ -12,8 +11,17 @@ func _ready():
 	position = Vector2(644, 347)
 	velocity = Vector2(SPEED, 0).rotated(rotation)
 
-# Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
+
+
+func _on_area_2d_body_entered(body):
+	pass
+#	velocity = velocity.bounce(Vector2(-1,0))
+
+
+func _on_area_2d_2_body_entered(body):
+	pass
+#	velocity = velocity.bounce(Vector2(-1,0))
